b08c7472c9b4b8601f8f0bf0d5f9462e93cdb817
Tim Miller <Tim@TimRCM.com>
2018-07-01 13:49:40 -0600

    framework for email notifications

---
+++ blankconfig.py
@@ -0,0 +1,12 @@
+# Auth token goes here
+dbxAccount = ''
+
+# Hostname, port, authentication, etc for SMTP notifications 
+
+smtp_host = ''
+smtp_port = 25
+smtp_auth_req = 0 # Set to '1' if authentication is required 
+smtp_username = ''
+smtp_password = ''
+smtp_sendto = ''
+smtp_sendfrom = ''
--- main.py
+++ main.py
@@ -2,10 +2,47 @@
 # Takes an environment path and backs it up to Dropbox
 # 7/1/2018 TimRCM
 
-import dropbox 
 from sys import argv
+import datetime
+import smtplib
+
+import dropbox 
 
 import config 
 
 path = argv
 
+dbx = dropbox.Dropbox(config.dbxAccount)
+dbx.users_get_current_account()
+print(dbx.users_get_current_account())
+
+
+def backup():
+    pass
+
+def notification():
+    ''' Sends an email notification when the backup fails for any reason.'''
+
+    message = f"""From: DirectoryBackup <{config.smtp_sendfrom}>
+        To: <{config.smtp_sendto}>
+        Subject: SMTP e-mail test
+
+        This is a test e-mail notification.
+
+    """
+
+    # Istantiate smtplib & log in if needed 
+    # SMTP_SSL is used here -- allow configuration for insecure SMTP servers later?
+    try: 
+        notify = smtplib.SMTP_SSL(host=config.smtp_host, port=config.smtp_port)
+        if config.smtp_auth_req == 1:
+            notify.login(config.smtp_username, config.smtp_password)
+        notify.sendmail(config.smtp_sendfrom, config.smtp_sendto, message)
+        print("Message sent... maybe.")
+
+    except:
+        print("Failed to send message.")
+        exit(1)
+    
+if __name__ == '__main__':
+    notification()
